1e6c55b98e8ada901054e9ff9fe561bb30a1f96f
Prithvi Okade <prithviro@fb.com>
2020-07-07 18:08:12 -0400

    compose fragment

--- app/src/main/java/com/example/instagramapp/MainActivity.java
+++ app/src/main/java/com/example/instagramapp/MainActivity.java
@@ -35,13 +35,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
-    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 112;
-    public String photoFileName = "photo.jpg";
-    File photoFile;
-    EditText etCaption;
-    Button btnCamera;
-    Button btnPost;
-    ImageView ivPhoto;
+
     BottomNavigationView bottomNavigation;
 
     @Override
@@ -49,37 +43,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etCaption = findViewById(R.id.etCaption);
-        btnCamera = findViewById(R.id.btnCamera);
-        btnPost = findViewById(R.id.btnPost);
-        ivPhoto = findViewById(R.id.ivPhoto);
         bottomNavigation = findViewById(R.id.bottomNavigation);
-
-        // queryPosts();
-
-        btnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchCamera();
-            }
-        });
-
-        btnPost.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String caption = etCaption.getText().toString();
-                if (caption.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Caption cannot be empty", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (photoFile == null || ivPhoto.getDrawable() == null) {
-                    Toast.makeText(MainActivity.this, "Image  cannot be empty", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                savePost(caption, currentUser, photoFile);
-            }
-        });
 
         bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -98,90 +62,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // Returns the File for a photo stored on disk given the fileName
-    public File getPhotoFileUri(String fileName) {
-        // Get safe storage directory for photos
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(TAG, "failed to create directory");
-        }
-
-        // Return the file target for the photo based on filename
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-        return file;
     }
-
-    private void launchCamera() {
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference for future access
-        photoFile = getPhotoFileUri(photoFileName);
-
-        // wrap File object into a content provider
-        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                // by this point we have the camera photo on disk
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                // Load the taken image into a preview
-                ImageView ivPreview = (ImageView) findViewById(R.id.ivPhoto);
-                ivPreview.setImageBitmap(takenImage);
-            } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void savePost(String caption, ParseUser user, File photoFile) {
-        Post post = new Post();
-        post.setCaption(caption);
-        post.setImage(new ParseFile(photoFile));
-        post.setUser(user);
-        post.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e != null) {
-                    Log.e(TAG, "Error while saving new post", e);
-                } else {
-                    Log.i(TAG, "Success saving new post");
-                    etCaption.setText("");
-                    ivPhoto.setImageResource(0);
-                }
-            }
-        });
-    }
-
-    private void queryPosts() {
-        // Specify which class to query
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        query.include(Post.KEY_USER);
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> posts, ParseException e) {
-                if (e != null) {
-                    Log.e(TAG, "Error while getting posts");
-                    return;
-                }
-                 for (Post post : posts) {
-                     Log.i(TAG, "Post: " + post.getCaption() + " User: " + post.getUser().getUsername());
-                 }
-            }
-        });
-
-    }
-}
